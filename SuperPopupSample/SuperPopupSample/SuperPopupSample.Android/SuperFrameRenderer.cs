cfc0e10bc14bba62c2f681fdc06829ccdf26d6da
Yegor Gromadskyi <yhrom@softserveinc.com>
2017-06-15 15:42:38 +0300

    Android

--- SuperPopupSample/SuperPopupSample/SuperPopupSample.Android/SuperFrameRenderer.cs
+++ SuperPopupSample/SuperPopupSample/SuperPopupSample.Android/SuperFrameRenderer.cs
@@ -8,12 +8,13 @@ using Android.Graphics;
 [assembly: ExportRenderer(typeof(SuperFrame), typeof(SuperFrameRenderer))]
 namespace SuperPopupSample.Droid
 {
-    public class SuperFrameRenderer : FrameRenderer
+    public sealed class SuperFrameRenderer : FrameRenderer
     {
         const double ArrowSize = 10;
 
-        SuperFrame _superFrame;
-        ArrowPlacement _arrowOptions;
+        SuperFrame superFrame;
+
+        bool drawArrow;
 
         protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
         {
@@ -21,46 +22,68 @@ namespace SuperPopupSample.Droid
 
             if (e.OldElement != null)
             {
-                _superFrame.DrawArrowRequested -= OnDrawArrowRequest;
+                superFrame.DrawArrowRequested -= OnDrawArrowRequest;
 
-                _superFrame = null;
-                _arrowOptions = null;
+                superFrame = null;
             }
 
             if (e.NewElement != null)
             {
-                _superFrame = e.NewElement as SuperFrame;
+                superFrame = e.NewElement as SuperFrame;
 
-                _superFrame.DrawArrowRequested += OnDrawArrowRequest;
+                superFrame.DrawArrowRequested += OnDrawArrowRequest;
             }
         }
 
-        void OnDrawArrowRequest(object sender, ArrowPlacement request)
+        void OnDrawArrowRequest(object sender, EventArgs args)
         {
-            DrawArrow(request);
-        }
-
-        void DrawArrow(ArrowPlacement request)
-        {
-            _arrowOptions = request;
+            drawArrow = true;
         }
 
         protected override void OnDraw(Canvas canvas)
         {
-            if (_arrowOptions != null)
+            if (drawArrow)
             {
                 var paint = new Paint()
                 {
-                    Color =  _superFrame.ArrowColor.ToAndroid()
+                    Color =  superFrame.ArrowColor.ToAndroid()
                 };
 
-                var x = Helpers.DpToPx(_arrowOptions.Location.X, ViewGroup.Context.Resources.DisplayMetrics);
-                var y = Helpers.DpToPx(_arrowOptions.Location.Y, ViewGroup.Context.Resources.DisplayMetrics);
+                var angle = 0f;
+                var x = 0d;
+                var y = 0d;
+
+                switch (superFrame.HorizontalArrowAlignment)
+                {
+                    case HorizontalAlignment.Left:
+                        x = ArrowSize * 1.5;
+                        break;
+
+                    case HorizontalAlignment.Center:
+                        x = superFrame.Width / 2 + ArrowSize / 2;
+                        break;
+
+                    case HorizontalAlignment.Right:
+                        x = superFrame.Width - ArrowSize * 1.5;
+                        break;
+                }
+
+                switch (superFrame.ArrowDirection)
+                {
+                    case ArrowDirection.Up:
+                        y = -ArrowSize;
+                        break;
+                    case ArrowDirection.Down:
+                        y = superFrame.Height + ArrowSize;
+                        angle = 180f;
+                        break;
+                }
+
+                var convertedX = Helpers.DpToPx(x, ViewGroup.Context.Resources.DisplayMetrics);
+                var convertedY = Helpers.DpToPx(y, ViewGroup.Context.Resources.DisplayMetrics);
                 var size = Helpers.DpToPx(ArrowSize, ViewGroup.Context.Resources.DisplayMetrics);
 
-                var angle = _arrowOptions.Direction == ArrowDirection.Down ? 180f : 0f;
-
-                DrawTriangle(canvas, paint, (float)x, (float)y, (float)size, angle);
+                DrawTriangle(canvas, paint, (float)convertedX, (float)convertedY, (float)size, angle);
             }
 
             base.OnDraw(canvas);
